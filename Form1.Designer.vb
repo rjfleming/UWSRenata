f6b5f50a67b7bd531a6c2e846e74ec93a3c3b12a
Richard Fleming <richardjayfleming@gmail.com>
2021-01-15 23:18:40 +0000

    Cleaning up and adding correct vb files

---
+++ Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBrowser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RefreshButton = New System.Windows.Forms.Button()
+        Me.HomeButton = New System.Windows.Forms.Button()
+        Me.FavsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.BookmarkLabel = New System.Windows.Forms.Label()
+        Me.BookmarkDateDisplay = New System.Windows.Forms.TextBox()
+        Me.AddButton = New System.Windows.Forms.Button()
+        Me.BookmarkCombBox = New System.Windows.Forms.ComboBox()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.diaSaveFile = New System.Windows.Forms.SaveFileDialog()
+        Me.diaOpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.ForwardButton = New System.Windows.Forms.Button()
+        Me.BackButton = New System.Windows.Forms.Button()
+        Me.WebBrowser = New System.Windows.Forms.WebBrowser()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.TxtUrlBox = New System.Windows.Forms.TextBox()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.FavsGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RefreshButton
+        '
+        Me.RefreshButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.RefreshButton.Location = New System.Drawing.Point(193, 139)
+        Me.RefreshButton.Name = "RefreshButton"
+        Me.RefreshButton.Size = New System.Drawing.Size(139, 45)
+        Me.RefreshButton.TabIndex = 0
+        Me.RefreshButton.Text = "Refresh"
+        Me.RefreshButton.UseVisualStyleBackColor = True
+        '
+        'HomeButton
+        '
+        Me.HomeButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.HomeButton.Location = New System.Drawing.Point(42, 139)
+        Me.HomeButton.Name = "HomeButton"
+        Me.HomeButton.Size = New System.Drawing.Size(139, 45)
+        Me.HomeButton.TabIndex = 1
+        Me.HomeButton.Text = "Home"
+        Me.HomeButton.UseVisualStyleBackColor = True
+        '
+        'FavsGroupBox
+        '
+        Me.FavsGroupBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FavsGroupBox.Controls.Add(Me.BookmarkLabel)
+        Me.FavsGroupBox.Controls.Add(Me.BookmarkDateDisplay)
+        Me.FavsGroupBox.Controls.Add(Me.AddButton)
+        Me.FavsGroupBox.Controls.Add(Me.BookmarkCombBox)
+        Me.FavsGroupBox.Controls.Add(Me.SaveButton)
+        Me.FavsGroupBox.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.FavsGroupBox.Location = New System.Drawing.Point(393, 75)
+        Me.FavsGroupBox.Name = "FavsGroupBox"
+        Me.FavsGroupBox.Size = New System.Drawing.Size(773, 118)
+        Me.FavsGroupBox.TabIndex = 2
+        Me.FavsGroupBox.TabStop = False
+        Me.FavsGroupBox.Text = "Favourites"
+        '
+        'BookmarkLabel
+        '
+        Me.BookmarkLabel.AutoSize = True
+        Me.BookmarkLabel.Location = New System.Drawing.Point(26, 78)
+        Me.BookmarkLabel.Name = "BookmarkLabel"
+        Me.BookmarkLabel.Size = New System.Drawing.Size(122, 18)
+        Me.BookmarkLabel.TabIndex = 8
+        Me.BookmarkLabel.Text = "Bookmark Date"
+        '
+        'BookmarkDateDisplay
+        '
+        Me.BookmarkDateDisplay.Location = New System.Drawing.Point(167, 78)
+        Me.BookmarkDateDisplay.Name = "BookmarkDateDisplay"
+        Me.BookmarkDateDisplay.Size = New System.Drawing.Size(306, 25)
+        Me.BookmarkDateDisplay.TabIndex = 7
+        Me.BookmarkDateDisplay.Tag = ""
+        '
+        'AddButton
+        '
+        Me.AddButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.AddButton.Location = New System.Drawing.Point(518, 17)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Size = New System.Drawing.Size(139, 45)
+        Me.AddButton.TabIndex = 6
+        Me.AddButton.Text = "Add"
+        Me.AddButton.UseVisualStyleBackColor = True
+        '
+        'BookmarkCombBox
+        '
+        Me.BookmarkCombBox.FormattingEnabled = True
+        Me.BookmarkCombBox.Location = New System.Drawing.Point(19, 25)
+        Me.BookmarkCombBox.Name = "BookmarkCombBox"
+        Me.BookmarkCombBox.Size = New System.Drawing.Size(454, 26)
+        Me.BookmarkCombBox.TabIndex = 2
+        '
+        'SaveButton
+        '
+        Me.SaveButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.SaveButton.Location = New System.Drawing.Point(518, 70)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(139, 45)
+        Me.SaveButton.TabIndex = 5
+        Me.SaveButton.Text = "Save"
+        Me.SaveButton.UseVisualStyleBackColor = True
+        '
+        'diaOpenFile
+        '
+        Me.diaOpenFile.FileName = "OpenFileDialog1"
+        '
+        'ForwardButton
+        '
+        Me.ForwardButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.ForwardButton.Location = New System.Drawing.Point(193, 75)
+        Me.ForwardButton.Name = "ForwardButton"
+        Me.ForwardButton.Size = New System.Drawing.Size(139, 45)
+        Me.ForwardButton.TabIndex = 3
+        Me.ForwardButton.Text = "Forward"
+        Me.ForwardButton.UseVisualStyleBackColor = True
+        '
+        'BackButton
+        '
+        Me.BackButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BackButton.Location = New System.Drawing.Point(42, 75)
+        Me.BackButton.Name = "BackButton"
+        Me.BackButton.Size = New System.Drawing.Size(139, 45)
+        Me.BackButton.TabIndex = 4
+        Me.BackButton.Text = "Back"
+        Me.BackButton.UseVisualStyleBackColor = True
+        '
+        'WebBrowser
+        '
+        Me.WebBrowser.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser.Location = New System.Drawing.Point(12, 219)
+        Me.WebBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser.Name = "WebBrowser"
+        Me.WebBrowser.ScriptErrorsSuppressed = True
+        Me.WebBrowser.Size = New System.Drawing.Size(1158, 453)
+        Me.WebBrowser.TabIndex = 5
+        Me.WebBrowser.Url = New System.Uri("http://www.bbc.co.uk", System.UriKind.Absolute)
+        '
+        'GoButton
+        '
+        Me.GoButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.GoButton.Location = New System.Drawing.Point(727, 4)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(139, 45)
+        Me.GoButton.TabIndex = 6
+        Me.GoButton.Text = "Go"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'TxtUrlBox
+        '
+        Me.TxtUrlBox.Location = New System.Drawing.Point(25, 13)
+        Me.TxtUrlBox.Name = "TxtUrlBox"
+        Me.TxtUrlBox.Size = New System.Drawing.Size(609, 26)
+        Me.TxtUrlBox.TabIndex = 7
+        '
+        'closeButton
+        '
+        Me.closeButton.Font = New System.Drawing.Font("Modern No. 20", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.closeButton.Location = New System.Drawing.Point(960, 4)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(139, 45)
+        Me.closeButton.TabIndex = 8
+        Me.closeButton.Text = "Close"
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'frmBrowser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1279, 697)
+        Me.Controls.Add(Me.closeButton)
+        Me.Controls.Add(Me.TxtUrlBox)
+        Me.Controls.Add(Me.GoButton)
+        Me.Controls.Add(Me.WebBrowser)
+        Me.Controls.Add(Me.RefreshButton)
+        Me.Controls.Add(Me.HomeButton)
+        Me.Controls.Add(Me.BackButton)
+        Me.Controls.Add(Me.ForwardButton)
+        Me.Controls.Add(Me.FavsGroupBox)
+        Me.Name = "frmBrowser"
+        Me.Text = "Browser"
+        Me.FavsGroupBox.ResumeLayout(False)
+        Me.FavsGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RefreshButton As Button
+    Friend WithEvents HomeButton As Button
+    Friend WithEvents FavsGroupBox As GroupBox
+    Friend WithEvents BookmarkCombBox As ComboBox
+    Friend WithEvents diaSaveFile As SaveFileDialog
+    Friend WithEvents diaOpenFile As OpenFileDialog
+    Friend WithEvents ForwardButton As Button
+    Friend WithEvents BackButton As Button
+    Friend WithEvents SaveButton As Button
+    Friend WithEvents AddButton As Button
+    Friend WithEvents WebBrowser As WebBrowser
+    Friend WithEvents GoButton As Button
+    Friend WithEvents TxtUrlBox As TextBox
+    Friend WithEvents BookmarkLabel As Label
+    Friend WithEvents BookmarkDateDisplay As TextBox
+    Friend WithEvents closeButton As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,214 @@
+﻿Public Class frmBrowser
+
+    Private Bookmarks(100) As Bookmark
+    Dim favouritesFileLocation As String
+
+    <Serializable()> Structure Bookmark
+        Dim savedDate As Date
+        Dim websiteAddress As String
+        Dim bookmarkDesc As String
+
+        'Create new bookmark
+        Public Sub New(ByVal desc As String, ByVal todaysDate As Date, ByVal websiteAddr As String)
+            bookmarkDesc = desc
+            savedDate = todaysDate
+            websiteAddress = websiteAddr
+        End Sub
+    End Structure
+
+    Private Sub resizeFormContents()
+        Dim borderIndent As Integer
+        Dim goBtn_X As Integer
+        Dim saveBtn_X As Integer
+        Dim addBtn_X As Integer
+        Dim closeBtn_X As Integer
+
+        borderIndent = WebBrowser.Location.X
+
+
+        addBtn_X = Me.Size.Width - AddButton.Width - borderIndent * 40
+        AddButton.Location = New Point(addBtn_X, AddButton.Location.Y)
+
+
+        saveBtn_X = Me.Size.Width - SaveButton.Width - borderIndent * 40
+        SaveButton.Location = New Point(saveBtn_X, SaveButton.Location.Y)
+
+
+        TxtUrlBox.Width = GoButton.Location.X - borderIndent * 4
+
+
+        WebBrowser.Width = Me.Size.Width - borderIndent * 4
+        WebBrowser.Height = Me.Size.Height - WebBrowser.Location.Y - borderIndent * 5
+
+        goBtn_X = Me.Size.Width - GoButton.Width - borderIndent * 35
+        GoButton.Location = New Point(goBtn_X, GoButton.Location.Y)
+
+        closeBtn_X = Me.Size.Width - closeButton.Width - borderIndent * 12
+        closeButton.Location = New Point(closeBtn_X, closeButton.Location.Y)
+
+
+        FavsGroupBox.Width = Me.Size.Width - borderIndent * 40
+
+        BookmarkCombBox.Width = Me.Size.Width - borderIndent * 65
+
+    End Sub
+
+    Private Sub AddBtn_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+        Dim websiteAddress As String
+        Dim ComboIndex As Integer
+        Dim bookmarkDesc As String
+        Dim savedDate As Date
+
+        If TxtUrlBox.Text = "" Then
+            GoTo IgnoreEntry
+        End If
+
+        Dim ButtonPressed = MessageBox.Show("Are you sure you want to add " + TxtUrlBox.Text + "to your bookmarks", "Yes No Cancel", MessageBoxButtons.YesNoCancel)
+
+        'If the user decides to click No or Cancel on the pop up then we ignore the Add button press
+        'If they choose yes then we continue ahead and try to add the bookmark
+        Select Case ButtonPressed
+            Case vbYes
+                Exit Select
+            Case vbNo
+                GoTo IgnoreEntry
+            Case vbCancel
+                GoTo IgnoreEntry
+        End Select
+
+        savedDate = DateTime.Today
+        websiteAddress = TxtUrlBox.Text
+
+        bookmarkDesc = InputBox("Please provide a description for Bookmark")
+
+        If bookmarkDesc = "" Then
+            GoTo IgnoreEntry
+        End If
+
+        BookmarkCombBox.BeginUpdate()
+        BookmarkCombBox.Items.Add(bookmarkDesc)
+        BookmarkCombBox.Text = bookmarkDesc
+        BookmarkCombBox.EndUpdate()
+        ComboIndex = BookmarkCombBox.SelectedIndex
+
+        Bookmarks(ComboIndex) = New Bookmark(bookmarkDesc, savedDate, websiteAddress)
+IgnoreEntry:
+    End Sub
+
+
+    Private Sub txtURL_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtUrlBox.KeyPress
+        Dim keyAscii As Char
+
+        keyAscii = e.KeyChar
+        If (keyAscii = vbCr) Then
+            If TxtUrlBox.Text <> "" Then
+                Call WebBrowser.Navigate(TxtUrlBox.Text)
+            End If
+        End If
+    End Sub
+
+
+    Private Sub bookmarkCombBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles BookmarkCombBox.SelectedIndexChanged
+        Dim IndexValue As Integer
+        IndexValue = BookmarkCombBox.SelectedIndex
+        If (Bookmarks(IndexValue).websiteAddress <> "") Then
+            BookmarkDateDisplay.Text = Bookmarks(IndexValue).savedDate
+            WebBrowser.Navigate(Bookmarks(IndexValue).websiteAddress)
+        End If
+    End Sub
+
+    Private Sub SaveBtn_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+
+        Dim MemStream As New System.IO.MemoryStream()
+        Dim BufForm As New System.Runtime.Serialization.Formatters.Binary.BinaryFormatter()
+
+        BufForm.Serialize(MemStream, Bookmarks)
+        My.Computer.FileSystem.WriteAllBytes(favouritesFileLocation,
+            MemStream.GetBuffer(), False)
+    End Sub
+
+
+
+    Private Sub frmBrowser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Initialise the close button to be disabled but as soon as other buttons are pressed this will be enabled
+        closeButton.Enabled = False
+
+        resizeFormContents()
+
+        favouritesFileLocation = "C:\UWSCourseWork\favourites.fws"
+        My.Computer.FileSystem.WriteAllText("C:\UWSCourseWork\history.hlf",
+            Date.Today.ToString & vbCrLf, True)
+        Dim BufForm As New System.Runtime.Serialization.Formatters.Binary.BinaryFormatter()
+        Dim MemStream As New System.IO.MemoryStream()
+
+        Dim DataStream As Byte()
+        Dim Index As Integer
+
+        If My.Computer.FileSystem.FileExists(favouritesFileLocation) Then
+
+            DataStream = My.Computer.FileSystem.ReadAllBytes(favouritesFileLocation)
+
+            Bookmarks = BufForm.Deserialize(New System.IO.MemoryStream(DataStream))
+
+            Index = 0
+            BookmarkCombBox.BeginUpdate()
+            While (Bookmarks(Index).bookmarkDesc <> "")
+                BookmarkCombBox.Items.Add(Bookmarks(Index).bookmarkDesc)
+                Index = Index + 1
+            End While
+            BookmarkCombBox.EndUpdate()
+        End If
+    End Sub
+
+    Private Sub backBtn_Click(sender As Object, e As EventArgs) Handles BackButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+        WebBrowser.GoBack()
+    End Sub
+
+    Private Sub homeBtn_Click(sender As Object, e As EventArgs) Handles HomeButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+        WebBrowser.Navigate("www.bbc.com")
+    End Sub
+
+    Private Sub fwdBtn_Click(sender As Object, e As EventArgs) Handles ForwardButton.Click
+        closeButton.Enabled = True
+        WebBrowser.GoForward()
+    End Sub
+
+    Private Sub goBtn_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+        WebBrowser.Navigate(TxtUrlBox.Text)
+    End Sub
+
+    Private Sub refreshBtn_Click(sender As Object, e As EventArgs) Handles RefreshButton.Click
+        'The close button can now be pressed
+        closeButton.Enabled = True
+        WebBrowser.Refresh()
+    End Sub
+
+    Private Sub webBrowser_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser.DocumentCompleted
+        TxtUrlBox.Text = WebBrowser.Url.ToString
+        Dim lines() As String = IO.File.ReadAllLines("C:\UWSCourseWork\history.hlf")
+        If TxtUrlBox.Text <> lines(lines.Length - 1) Then
+            My.Computer.FileSystem.WriteAllText("C:\UWSCourseWork\history.hlf",
+            TxtUrlBox.Text & vbCrLf, True)
+        End If
+    End Sub
+
+    Private Sub frmBrowser_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        resizeFormContents()
+    End Sub
+
+
+    'The close button should only work if some other has been pressed
+    Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+        End
+    End Sub
+End Class
